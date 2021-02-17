224d08a6bdeb1de3792dad353f151926f78332f1
alec wayman <aejw_edu@pm.me>
2021-02-17 12:07:01 +0000

    adding more and now correct validations to restaurant and review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   cuisines = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
   validates :category, inclusion: { in: cuisines, message: "%{value} is not a valid category" }
-  validates :rating, :inclusion => 0..5
+
+  has_many :reviews, dependent: :destroy
 end
