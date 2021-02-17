9c8d0ca643f55b657900630781a87293d201eed3
alec wayman <aejw_edu@pm.me>
2021-02-17 12:02:33 +0000

    adding validations to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  cuisines = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, presence: true
+  validates :category, inclusion: { in: cuisines, message: "%{value} is not a valid category" }
+  validates :rating, :inclusion => 0..5
 end
